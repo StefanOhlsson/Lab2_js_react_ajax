ccbf18bf8f1c19836cb0b58b15dd7f07d4e8f84c
StefanOhlsson <stefan.ohlsson.777@gmail.com>
2019-04-12 10:13:49 +0200

    Updates

---
+++ precache-manifest.20c5a07d5abbbf91769d1cbfdbd156ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ba13fd8bce329873104f7f297cad152",
+    "url": "/lab2_js_react_ajax/static/media/books3.9ba13fd8.jpg"
+  },
+  {
+    "revision": "2e41c289ff937745b7b0",
+    "url": "/lab2_js_react_ajax/static/js/runtime~main.f5348f5e.js"
+  },
+  {
+    "revision": "48eb0c66a7b87212b7f6",
+    "url": "/lab2_js_react_ajax/static/js/main.e22aa0e8.chunk.js"
+  },
+  {
+    "revision": "b0071d16893ca23f9881",
+    "url": "/lab2_js_react_ajax/static/js/2.2c94bd99.chunk.js"
+  },
+  {
+    "revision": "48eb0c66a7b87212b7f6",
+    "url": "/lab2_js_react_ajax/static/css/main.6cb191b1.chunk.css"
+  },
+  {
+    "revision": "47ccc8ba86f850c00cfa20d1b83ade13",
+    "url": "/lab2_js_react_ajax/index.html"
+  }
+];
